da43c9361f9cdbf7eb69ecdfb7bd25fedcbf74f5
grayledw <grayledw@rose-hulman.edu>
2017-10-09 13:00:20 -0400

    working on m2

--- src/m2_more_sequences.py
+++ src/m2_more_sequences.py
@@ -19,8 +19,8 @@ def main():
     run_test_shortest_string()
     run_test_index_of_largest_number()
     run_test_number_of_stutters()
-    # run_test_is_palindrome()
-    # run_test_count_same()
+    run_test_is_palindrome()
+    run_test_count_same()
 
 
 # ----------------------------------------------------------------------
@@ -325,7 +325,7 @@ def is_palindrome(s):
       :type s: str
     """
     # ------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # Done: 5. Implement and test this function.
     #     The testing code is already written for you (above).
     #
     ####################################################################
@@ -334,6 +334,10 @@ def is_palindrome(s):
     #   above are particularly good examples to work by hand.
     ####################################################################
     # ------------------------------------------------------------------
+    for k in range(len(s)):
+        if s[k] != s[len(s) - k - 1]:
+            return False
+    return True
 
 
 # ----------------------------------------------------------------------
@@ -392,9 +396,16 @@ def count_same(sequence1, sequence2):
       type: sequence2: tuple or list or string
     """
     # ------------------------------------------------------------------
-    # TODO: 6. Implement and test this function.
+    # Done: 6. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
+
+    number_indices = 0
+    for k in range(len(sequence1)):
+        if sequence1[k] == sequence2[k]:
+            number_indices = number_indices + 1
+
+    return number_indices
 
 
 # ----------------------------------------------------------------------
