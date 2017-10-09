7964c92dddd7613fc2de5a0f7560bb1c30eefc0d
grayledw <grayledw@rose-hulman.edu>
2017-10-09 12:39:04 -0400

    working on m2

--- src/m2_more_sequences.py
+++ src/m2_more_sequences.py
@@ -18,7 +18,7 @@ def main():
     """ Calls the   TEST   functions in this module. """
     run_test_shortest_string()
     run_test_index_of_largest_number()
-    # run_test_number_of_stutters()
+    run_test_number_of_stutters()
     # run_test_is_palindrome()
     # run_test_count_same()
 
@@ -246,9 +246,15 @@ def number_of_stutters(s):
        :type s: str
     """
     # ------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # Done: 4. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
+
+    number_of_stutter = 0
+    for k in range(len(s) - 1):
+        if s[k + 1] == s[k]:
+            number_of_stutter = number_of_stutter + 1
+    return number_of_stutter
 
 
 def run_test_is_palindrome():
